ee2eef6295950bb1a488970c6114b4d33ce94a5b
simcard0000 <43153413+simcard0000@users.noreply.github.com>
2020-12-02 06:37:17 -0500

    day 2 complete

--- src/advent-of-code-2020.cpp
+++ src/advent-of-code-2020.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctype.h>
 using namespace std;
 
 int day1A () {
@@ -63,6 +64,109 @@ int day1B () {
 	return answer;
 }
 
+int day2A() {
+	int answer = 0;
+	vector<string> passwords;
+	//reading in the input:
+	string line;
+	while (getline(cin, line)){
+		passwords.push_back(line);
+	}
+	//counting the number of valid passwords:
+	int size = (int) passwords.size();
+	for (int i = 0; i < size; i++){
+		int placeholder = 0;
+		int low;
+		int high;
+		char letter;
+		string password;
+		string create;
+
+		while (isdigit(passwords[i].at(placeholder))){
+			create += passwords[i].at(placeholder);
+			placeholder++;
+		}
+		low = atoi(create.c_str());
+		placeholder++;
+		create.clear();
+		while (isdigit(passwords[i].at(placeholder))){
+			create += passwords[i].at(placeholder);
+			placeholder++;
+		}
+		high = atoi(create.c_str());
+		placeholder++;
+
+		letter = passwords[i].at(placeholder);
+		placeholder += 3;
+		password = passwords[i].substr(placeholder).c_str();
+
+		int length = (int) password.length();
+		int counter = 0;
+		for (int j = 0; j < length; j++){
+			if (password.at(j) == letter){
+				counter++;
+			}
+		}
+		if (counter >= low && counter <= high){
+			answer++;
+		}
+	}
+	return answer;
+}
+
+int day2B() {
+	int answer = 0;
+	vector<string> passwords;
+	//reading in the input:
+	string line;
+	while (getline(cin, line)){
+		passwords.push_back(line);
+	}
+	//counting the number of valid passwords:
+	int size = (int) passwords.size();
+	for (int i = 0; i < size; i++){
+		int placeholder = 0;
+		int firstPos;
+		int lastPos;
+		char letter;
+		string password;
+		string create;
+
+		while (isdigit(passwords[i].at(placeholder))){
+			create += passwords[i].at(placeholder);
+			placeholder++;
+		}
+		firstPos = atoi(create.c_str());
+		placeholder++;
+		create.clear();
+		while (isdigit(passwords[i].at(placeholder))){
+			create += passwords[i].at(placeholder);
+			placeholder++;
+		}
+		lastPos = atoi(create.c_str());
+		placeholder++;
+
+		letter = passwords[i].at(placeholder);
+		placeholder += 3;
+		password = passwords[i].substr(placeholder).c_str();
+
+		firstPos--;
+		lastPos--;
+		bool atFirstPos = false;
+		bool atLastPos = false;
+		if (password.at(firstPos) == letter){
+			atFirstPos = true;
+		}
+		if (password.at(lastPos) == letter){
+			atLastPos = true;
+		}
+		if ((!atFirstPos && atLastPos) || (atFirstPos && !atLastPos) ){
+			answer++;
+		}
+	}
+	return answer;
+}
+
 int main() {
 	/* Functions for printing answers out,
 	 * comment out function and print statements
@@ -71,7 +175,11 @@ int main() {
 
 	//int day1A_answer = day1A();
 	//int day1B_answer = day1B();
+	//int day2A_answer = day2A();
+	//int day2B_answer = day2B();
 	//cout << "day1A answer: " + to_string(day1A_answer) << endl;
 	//cout << "day1B answer: " + to_string(day1B_answer) << endl;
+	//cout<< "day2A answer: " + to_string(day2A_answer) << endl;
+	//cout << "day2B answer: " + to_string(day2B_answer) << endl;
 	return 0;
 }
